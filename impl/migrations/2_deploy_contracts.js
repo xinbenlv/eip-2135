5e79269627f02c610ba42c3076f79e672c88b16a
Zainan Victor Zhou <zzn@zzn.im>
2019-07-14 18:35:18 -0700

    Fix typo

--- impl/migrations/2_deploy_contracts.js
+++ impl/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var SimpleEIP2135Ticket = artifacts.require("./SimpleEIP2135Ticket.sol");
 var Ticket721 = artifacts.require("./Ticket721.sol");
 module.exports = function(deployer) {
-  deployer.deploy(SimpleEIp2135Ticket);
+  deployer.deploy(SimpleEIP2135Ticket);
   deployer.deploy(Ticket721);
 };
